98260a71ba58a87d26b8d95394628189c70cc9b6
druvvv <dmittal1902@gmail.com>
2022-10-30 04:15:43 +0530

    new

--- scipt.js
+++ scipt.js
@@ -1,69 +1,42 @@
 let handle;
 let ip;
 let card = document.getElementById("cardHolder");
-
+let TimeOffset;
 //alert("The website changes into dark and light mode in the backend but i was too lazy and tired of doing all the CSS so please just assume that the frontend changed.\nThe dark night mode toggle button has also been given for user convinience. The proofs of mode change will be visible to...")
-//--------------------------------------------getIP()-----------------------------------------------;
+getIP();
 
-const date = new Date();
-let CurrentTime = date.getTime();
-console.log(CurrentTime);
-const date2 = new Date(CurrentTime);
-var CurrentHour = date2.getHours();
-
-console.log(CurrentHour);
-let x = document.getElementById("cfff");
-x.setAttribute("placeholder" , CurrentHour);
 
 let buttonElement = document.getElementById("darkBtn");
 
-buttonElement.addEventListener('click' , darklightchanger);
-if(CurrentHour>=18 || CurrentHour <6){
+buttonElement.addEventListener('click', darklightchanger);
+
+function darklightchanger() {
     let mode = document.getElementById("darklight");
-    if(mode == null){
+    if (mode == null) {
         mode = document.getElementById("lightdark");
         mode.id = "darklight";
-        mode.setAttribute("src" , "resources/darkmode.png");
-       
-    }
+        mode.setAttribute("src", "resources/darkmode.png");
 
-}
-else{
-    let mode = document.getElementById("lightdark");
-    if(mode == null){
-        mode = document.getElementById("darklight");
+    }
+    else {
         mode.id = "lightdark";
-        mode.setAttribute("src" , "resources/sun.png");
-       
-    }
-}
-function darklightchanger(){
-    let mode = document.getElementById("darklight");
-    if(mode == null){
-        mode = document.getElementById("lightdark");
-        mode.id = "darklight";
-        mode.setAttribute("src" , "resources/darkmode.png");
-       
-    }
-    else{
-        mode.id = "lightdark";
-        mode.setAttribute("src" , "resources/sun.png");
+        mode.setAttribute("src", "resources/sun.png");
     }
 }
 
 
 
-function readText(form){
-   handle = form.cf_id.value;
-   let URL = "https://codeforces.com/api/user.info?handles=" + handle;
-   getInfo(URL , handle);
+function readText(form) {
+    handle = form.cf_id.value;
+    let URL = "https://codeforces.com/api/user.info?handles=" + handle;
+    getInfo(URL, handle);
 
-   card.style.display = "flex";
+    card.style.display = "flex";
 }
 
-function getIP(){
+function getIP() {
     let URL = 'https://api.ipgeolocation.io/getip';
-   
+
     fetch(URL).then(response => {
         return response.json();
     }).then(json => {
@@ -72,47 +45,81 @@ function getIP(){
     })
 }
 
-function getLocation(){
+function getLocation() {
     let URL = "https://api.ipgeolocation.io/timezone?apiKey=9ae8fcec28c447718220502a9f0ac3ac&ip=" + ip;
 
     fetch(URL).then(response => {
         return response.json();
     }).then(json => {
-        let TimeOffset = json['timezone_offset'];
+        TimeOffset = json['timezone_offset'];
+        console.log(typeof (TimeOffset));
         let TimeZone = json['timezone'];
+        let NetTimeOffset = (TimeOffset - 5.5) * 3600000;
+        console.log(NetTimeOffset);
+        const date = new Date();
+        let CurrentTime = date.getTime();
+        let LocalCurrentTime = CurrentTime + NetTimeOffset;
+        console.log(CurrentTime);
+        const date2 = new Date(LocalCurrentTime);
+        let CurrentHour = date2.getHours();
+
+        console.log(CurrentHour);
+        let x = document.getElementById("cfff");
+        x.setAttribute("placeholder", CurrentHour);
+        if (CurrentHour >= 18 || CurrentHour < 6) {
+            let mode = document.getElementById("darklight");
+            if (mode == null) {
+                mode = document.getElementById("lightdark");
+                mode.id = "darklight";
+                mode.setAttribute("src", "resources/darkmode.png");
+        
+            }
+        
+        }
+        else {
+            let mode = document.getElementById("lightdark");
+            if (mode == null) {
+                mode = document.getElementById("darklight");
+                mode.id = "lightdark";
+                mode.setAttribute("src", "resources/sun.png");
+        
+            }
+        }
     })
 }
 
 
 
-function getInfo(URL , handle){
+function getInfo(URL, handle) {
     fetch(URL).then(resp => {
         return resp.json();
     }).then(
-         json => {
+        json => {
             let x = json['result'];
             let y = x[0]['titlePhoto'];
             let userImage = document.getElementById("userImage");
-            userImage.setAttribute("src" , y);
+            userImage.setAttribute("src", y);
 
             let userName = document.getElementById("userName");
-            if(x[0]['firstName'] == null){
+            if (x[0]['firstName'] == null) {
                 userName.innerText = "Anonymous";
             }
-            else{
-            userName.innerText = x[0]['firstName'] + " " + x[0]['lastName'];}
-            
+            else {
+                userName.innerText = x[0]['firstName'] + " " + x[0]['lastName'];
+            }
+
             let rating = x[0]['rating'];
 
             let userRating = document.getElementById("userRating");
             userRating.innerText = "Current Rating : " + rating;
 
             let userCountry = document.getElementById("userCountry");
-            if(x[0]['country'] == null){
+            if (x[0]['country'] == null) {
                 userCountry.innerText = "Country : Unknown";
             }
-            else{
-            userCountry.innerText = "Country : " + x[0]['country'];}
+            else {
+                userCountry.innerText = "Country : " + x[0]['country'];
+            }
 
             let userRank = document.getElementById("userRank");
             userRank.innerText = "Rank : " + x[0]['rank'];
@@ -122,7 +129,7 @@ function getInfo(URL , handle){
 
             let profile = document.getElementById("Profile");
             let link = "https://codeforces.com/profile/" + handle;
-            profile.setAttribute("href" , link)
-         }
+            profile.setAttribute("href", link)
+        }
     )
 }
